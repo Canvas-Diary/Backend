4e1a1e53df640a0fe99d368f31facc61c44237ea
kch1015 <songwoo1015@gmail.com>
2024-10-14 22:45:19 +0900

    feat: 트랜잭션 의존성 추가, 컴포넌트 스캔 지정

---
+++ Application-Module/src/main/java/com/canvas/application/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package com.canvas.application;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.canvas.application")
+public class ApplicationConfig {
+}
