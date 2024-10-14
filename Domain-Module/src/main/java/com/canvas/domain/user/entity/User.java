11e0b2a30302d955897b1d2b16a3ef7adcde64b3
kch1015 <songwoo1015@gmail.com>
2024-10-14 14:29:38 +0900

    feat: 도메인 클래스 수정

--- Domain-Module/src/main/java/com/canvas/domain/user/entity/User.java
+++ Domain-Module/src/main/java/com/canvas/domain/user/entity/User.java
@@ -1,5 +1,6 @@
 package com.canvas.domain.user.entity;
 
+import com.canvas.domain.user.enums.SocialLoginProvider;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,12 +10,13 @@ public class User {
     private final Long id;
     private final String email;
     private final String username;
+    private final SocialLoginProvider socialLoginProvider;
 
-    public static User withoutId(String email, String username) {
-        return new User(null, email, username);
+    public static User withoutId(String email, String username, SocialLoginProvider socialLoginProvider) {
+        return new User(null, email, username, socialLoginProvider);
     }
 
-    public static User withId(Long id, String email, String username) {
-        return new User(id, email, username);
+    public static User withId(Long id, String email, String username, SocialLoginProvider socialLoginProvider) {
+        return new User(id, email, username, socialLoginProvider);
     }
 }
