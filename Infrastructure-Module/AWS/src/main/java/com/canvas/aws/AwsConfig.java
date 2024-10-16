5eabb76a141b7cd26f23d3934036bda8db959862
전현민 <77275989+joyjhm@users.noreply.github.com>
2024-10-17 01:24:15 +0900

    chore: aws 모듈 초기 세팅

---
+++ Infrastructure-Module/AWS/src/main/java/com/canvas/aws/AwsConfig.java
@@ -0,0 +1,9 @@
+package com.canvas.aws;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.canvas.aws"})
+public class AwsConfig {
+}
