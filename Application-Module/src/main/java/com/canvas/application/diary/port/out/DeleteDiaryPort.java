eaae653ecdc394839017274128a615f66ff8b903
kch1015 <songwoo1015@gmail.com>
2024-10-16 19:48:33 +0900

    refactor: 포트 통합

--- Application-Module/src/main/java/com/canvas/application/diary/port/out/DeleteDiaryPort.java
+++
@@ -1,7 +0,0 @@
-package com.canvas.application.diary.port.out;
-
-import com.canvas.domain.diary.entity.Diary;
-
-public interface DeleteDiaryPort {
-    void delete(Diary diary);
-}
--- Application-Module/src/main/java/com/canvas/application/user/port/out/DeleteUserPort.java
+++
@@ -1,7 +0,0 @@
-package com.canvas.application.user.port.out;
-
-import com.canvas.domain.user.entity.User;
-
-public interface DeleteUserPort {
-    void delete(User user);
-}
