dd3dd84a0b7ce433c67c3729e58c48190951ac54
kch1015 <songwoo1015@gmail.com>
2024-10-16 19:49:05 +0900

    rename: 포트 이름 변경

--- Application-Module/src/main/java/com/canvas/application/image/port/out/ImageGenerationPort.java
+++ Application-Module/src/main/java/com/canvas/application/image/port/out/ImageGenerationPort.java
@@ -2,6 +2,6 @@ package com.canvas.application.image.port.out;
 
 import com.canvas.application.common.enums.Style;
 
-public interface GenerateImagePort {
+public interface ImageGenerationPort {
     String generate(String content, Style style);
 }
