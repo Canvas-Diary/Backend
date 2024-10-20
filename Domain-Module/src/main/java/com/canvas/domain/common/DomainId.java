ae0c65ba5686f9b7a4d4d29d631cd418343e0e85
kch1015 <songwoo1015@gmail.com>
2024-10-20 20:08:19 +0900

    feat: DomainId 생성자, equals, toString 추가

--- Domain-Module/src/main/java/com/canvas/domain/common/DomainId.java
+++ Domain-Module/src/main/java/com/canvas/domain/common/DomainId.java
@@ -1,7 +1,9 @@
 package com.canvas.domain.common;
 
+import com.github.f4b6a3.ulid.Ulid;
 import com.github.f4b6a3.ulid.UlidCreator;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public record DomainId(
@@ -9,5 +11,25 @@ public record DomainId(
 ) {
     public static DomainId generate() {
         return new DomainId(UlidCreator.getMonotonicUlid().toUuid());
+    }
+
+    public static DomainId from(String id) {
+        return new DomainId(Ulid.from(id).toUuid());
+    }
+
+    public static DomainId from(UUID uuid) {
+        return new DomainId(uuid);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (!(object instanceof DomainId domainId)) return false;
+        return Objects.equals(value, domainId.value);
+    }
+
+    @Override
+    public String toString() {
+        return value.toString();
     }
 }
