04d21150b8853007551bd5a327927f3061df6a08
전현민 <77275989+joyjhm@users.noreply.github.com>
2024-10-21 03:54:02 +0900

    fix: 이미지 stream 처리 수정

--- Infrastructure-Module/AWS/src/main/java/com/canvas/aws/AwsConfig.java
+++ Infrastructure-Module/AWS/src/main/java/com/canvas/aws/AwsConfig.java
@@ -1,9 +1,11 @@
 package com.canvas.aws;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = {"com.canvas.aws"})
+@EnableAutoConfiguration
 public class AwsConfig {
 }
