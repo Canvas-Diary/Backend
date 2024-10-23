542360a02b5313ade6b012bc1190cc75b75d77df
kch1015 <songwoo1015@gmail.com>
2024-10-23 17:49:27 +0900

    feat: Like Port 구현

---
+++ Infrastructure-Module/Persistence/src/main/java/com/canvas/persistence/jpa/diary/adapter/LikeManagementJpaAdapter.java
@@ -0,0 +1,36 @@
+package com.canvas.persistence.jpa.diary.adapter;
+
+import com.canvas.application.like.exception.LikeException;
+import com.canvas.application.like.out.LikeManagementPort;
+import com.canvas.domain.common.DomainId;
+import com.canvas.domain.diary.entity.Like;
+import com.canvas.persistence.jpa.diary.LikeMapper;
+import com.canvas.persistence.jpa.diary.repository.LikeJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class LikeManagementJpaAdapter implements LikeManagementPort {
+
+    private final LikeJpaRepository likeJpaRepository;
+
+    @Override
+    public void add(Like like) {
+        if (likeJpaRepository.existsByUserIdAndDiaryId(like.getUserId().value(), like.getDiaryId().value())) {
+            throw new LikeException.LikeAlreadyExistsException();
+        }
+
+        likeJpaRepository.save(LikeMapper.toEntity(like));
+    }
+
+    @Override
+    public void remove(DomainId userId, DomainId diaryId) {
+        if (!likeJpaRepository.existsByUserIdAndDiaryId(userId.value(), diaryId.value())) {
+            throw new LikeException.LikeNotFoundException();
+        }
+
+        likeJpaRepository.deleteByUserIdAndDiaryId(userId.value(), diaryId.value());
+    }
+
+}
