05773b38ca7aaa7cd49a4fb5a042c4cbee570d3b
kch1015 <songwoo1015@gmail.com>
2024-10-30 16:21:50 +0900

    feat: CORS 허용

--- Bootstrap-Module/src/main/java/com/canvas/bootstrap/common/config/WebConfig.java
+++ Bootstrap-Module/src/main/java/com/canvas/bootstrap/common/config/WebConfig.java
@@ -4,12 +4,20 @@ package com.canvas.bootstrap.common.config;
 import com.canvas.bootstrap.common.annotation.AccessUserArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new AccessUserArgumentResolver());
