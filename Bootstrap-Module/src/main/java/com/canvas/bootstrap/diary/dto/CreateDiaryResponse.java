2f82820991335350196a74ed477f816f44d8ab77
kch1015 <songwoo1015@gmail.com>
2024-10-31 15:14:39 +0900

    style: 오타 수정

--- Bootstrap-Module/src/main/java/com/canvas/bootstrap/diary/dto/CreateDiaryResponse.java
+++ Bootstrap-Module/src/main/java/com/canvas/bootstrap/diary/dto/CreateDiaryResponse.java
@@ -5,6 +5,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Schema(description = "일기 생성 응답")
 public record CreateDiaryResponse(
         @Schema(description = "생성된 일기 ID")
-        String DiaryId
+        String diaryId
 ) {
 }
