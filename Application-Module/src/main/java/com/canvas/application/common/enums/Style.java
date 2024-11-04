a77cc8966c7101b2bfe856581a8887faa1e6e61b
kch1015 <songwoo1015@gmail.com>
2024-11-04 15:40:20 +0900

    feat: Style 추가

--- Application-Module/src/main/java/com/canvas/application/common/enums/Style.java
+++ Application-Module/src/main/java/com/canvas/application/common/enums/Style.java
@@ -12,8 +12,11 @@ public enum Style {
     PHOTOREALISTIC("photorealistic", "실사화", "https://canvas-diary.s3.ap-northeast-2.amazonaws.com/%ED%99%94%ED%92%8D/%E1%84%89%E1%85%B5%E1%86%AF%E1%84%89%E1%85%A1%E1%84%92%E1%85%AA.webp"),
     OILPAINTING("oil painting", "유화", "https://canvas-diary.s3.ap-northeast-2.amazonaws.com/%ED%99%94%ED%92%8D/%E1%84%8B%E1%85%B2%E1%84%92%E1%85%AA.webp"),
     WATERCOLOR("watercolor", "수채화", "https://canvas-diary.s3.ap-northeast-2.amazonaws.com/%ED%99%94%ED%92%8D/%E1%84%89%E1%85%AE%E1%84%8E%E1%85%A2%E1%84%92%E1%85%AA.webp"),
-    ANIMATION("animation", "애니메이션", "https://canvas-diary.s3.ap-northeast-2.amazonaws.com/%ED%99%94%ED%92%8D/%E1%84%8B%E1%85%A2%E1%84%82%E1%85%B5%E1%84%86%E1%85%A6%E1%84%8B%E1%85%B5%E1%84%89%E1%85%A7%E1%86%AB+.webp"),
-    PIXELART("pixel art", "픽셀 아트", "https://canvas-diary.s3.ap-northeast-2.amazonaws.com/%ED%99%94%ED%92%8D/%E1%84%91%E1%85%B5%E1%86%A8%E1%84%89%E1%85%A6%E1%86%AF+%E1%84%8B%E1%85%A1%E1%84%90%E1%85%B3.webp");
+    ANIMATION("3D animation", "3D 애니메이션", "https://canvas-diary.s3.ap-northeast-2.amazonaws.com/%ED%99%94%ED%92%8D/%E1%84%8B%E1%85%A2%E1%84%82%E1%85%B5%E1%84%86%E1%85%A6%E1%84%8B%E1%85%B5%E1%84%89%E1%85%A7%E1%86%AB+.webp"),
+    PIXELART("retro graphics", "픽셀 아트", "https://canvas-diary.s3.ap-northeast-2.amazonaws.com/%ED%99%94%ED%92%8D/%E1%84%91%E1%85%B5%E1%86%A8%E1%84%89%E1%85%A6%E1%86%AF+%E1%84%8B%E1%85%A1%E1%84%90%E1%85%B3.webp"),
+    COMICS("American comics style", "코믹스", "https://canvas-diary.s3.ap-northeast-2.amazonaws.com/%ED%99%94%ED%92%8D/%E1%84%8F%E1%85%A9%E1%84%86%E1%85%B5%E1%86%A8%E1%84%89%E1%85%B3.jpeg"),
+    ABSTRACT("abstract", "추상화", "https://canvas-diary.s3.ap-northeast-2.amazonaws.com/%ED%99%94%ED%92%8D/%E1%84%8E%E1%85%AE%E1%84%89%E1%85%A1%E1%86%BC%E1%84%92%E1%85%AA.jpeg"),
+    ANIME("anime", "애니메이션", "https://canvas-diary.s3.ap-northeast-2.amazonaws.com/%ED%99%94%ED%92%8D/%E1%84%8B%E1%85%A2%E1%84%82%E1%85%B5%E1%84%86%E1%85%A6%E1%84%8B%E1%85%B5%E1%84%89%E1%85%A7%E1%86%AB.jpeg");
 
     private final String value;
     private final String koreanName;
