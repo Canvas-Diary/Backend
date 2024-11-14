334faf8aa1119c3f3015edc6b2aa30340b58670f
전현민 <77275989+joyjhm@users.noreply.github.com>
2024-11-14 20:49:00 +0900

    fix: 코드 리뷰 반영
    -#106

--- Bootstrap-Module/src/main/java/com/canvas/bootstrap/diary/dto/SaveKeywordRequest.java
+++ Bootstrap-Module/src/main/java/com/canvas/bootstrap/diary/dto/SaveKeywordRequest.java
@@ -2,14 +2,11 @@ package com.canvas.bootstrap.diary.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Schema(description = "키워드 저장 요청")
 public record SaveKeywordRequest(
         @Schema(description = "저장할 키워드 리스트")
-        List<String> keywords,
-        @Schema(description = "일기 날짜")
-        LocalDate date
+        List<String> keywords
 ) {
 }
