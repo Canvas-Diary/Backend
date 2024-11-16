acf346409966db7af9eb202b7f6bdf18978d2a35
전현민 <77275989+joyjhm@users.noreply.github.com>
2024-11-17 02:46:35 +0900

    feat: LLM을 이용한 기능 추상화
    -#106

--- Infrastructure-Module/Google/src/main/java/com/canvas/google/gemini/DiaryContentConvertGeminiAdapter.java
+++ Infrastructure-Module/Google/src/main/java/com/canvas/google/gemini/DiaryContentConvertGeminiAdapter.java
@@ -4,8 +4,8 @@ import com.canvas.application.diary.port.out.DiaryEmotionExtractPort;
 import com.canvas.application.diary.port.out.DiaryKeywordExtractPort;
 import com.canvas.application.image.port.out.ImagePromptGeneratePort;
 import com.canvas.domain.diary.enums.Emotion;
-import com.canvas.google.gemini.service.GeminiPromptConsts;
-import com.canvas.google.gemini.service.GeminiService;
+import com.canvas.google.gemini.service.LLMPromptConsts;
+import com.canvas.google.gemini.service.LLMService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -19,22 +19,22 @@ import java.util.List;
 public class DiaryContentConvertGeminiAdapter
         implements DiaryEmotionExtractPort, ImagePromptGeneratePort, DiaryKeywordExtractPort {
 
-    private final GeminiService geminiService;
+    private final LLMService llmService;
 
     @Override
     public Emotion emotionExtract(String content) {
-        String emotion = geminiService.generate(GeminiPromptConsts.EMOTION_EXTRACT + content);
+        String emotion = llmService.generate(LLMPromptConsts.EMOTION_EXTRACT + content);
         return Emotion.parse(emotion);
     }
 
     @Override
     public String generatePrompt(String content) {
-        return geminiService.generate(GeminiPromptConsts.IMAGE_GENERATOR + content);
+        return llmService.generate(LLMPromptConsts.IMAGE_GENERATOR + content);
     }
 
     @Override
     public List<String> keywordExtract(String content) {
-        String response = geminiService.generate(GeminiPromptConsts.KEYWORD_EXTRACT + content);
+        String response = llmService.generate(LLMPromptConsts.KEYWORD_EXTRACT + content);
         return Arrays.stream(response.split(", ")).toList();
     }
 }
