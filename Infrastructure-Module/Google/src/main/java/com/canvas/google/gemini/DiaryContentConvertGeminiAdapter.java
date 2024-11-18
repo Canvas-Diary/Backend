d6a7e0bd6916260f31ee33d8bf2e5019e347fb44
kch1015 <songwoo1015@gmail.com>
2024-11-19 03:49:44 +0900

    refactor: 프롬프트 상수 공통 사용으로 변경

--- Infrastructure-Module/Google/src/main/java/com/canvas/google/gemini/DiaryContentConvertGeminiAdapter.java
+++ Infrastructure-Module/Google/src/main/java/com/canvas/google/gemini/DiaryContentConvertGeminiAdapter.java
@@ -3,8 +3,8 @@ package com.canvas.google.gemini;
 import com.canvas.application.diary.port.out.DiaryEmotionExtractPort;
 import com.canvas.application.diary.port.out.DiaryKeywordExtractPort;
 import com.canvas.application.image.port.out.ImagePromptGeneratePort;
+import com.canvas.application.image.port.out.PromptConsts;
 import com.canvas.domain.diary.enums.Emotion;
-import com.canvas.google.gemini.service.GeminiPromptConsts;
 import com.canvas.google.gemini.service.GeminiService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,18 +23,18 @@ public class DiaryContentConvertGeminiAdapter
 
     @Override
     public Emotion emotionExtract(String content) {
-        String emotion = geminiService.generate(GeminiPromptConsts.EMOTION_EXTRACT + content);
+        String emotion = geminiService.generate(PromptConsts.EMOTION_EXTRACT + content);
         return Emotion.parse(emotion);
     }
 
     @Override
-    public String generatePrompt(String content, String weightedContent) {
-        return geminiService.generate(String.format(GeminiPromptConsts.IMAGE_GENERATOR, content, weightedContent));
+    public String generatePrompt(String content, String joinedWeightedContents) {
+        return geminiService.generate(String.format(PromptConsts.IMAGE_GENERATOR, content, joinedWeightedContents));
     }
 
     @Override
     public List<String> keywordExtract(String content) {
-        String response = geminiService.generate(GeminiPromptConsts.KEYWORD_EXTRACT + content);
+        String response = geminiService.generate(PromptConsts.KEYWORD_EXTRACT + content);
         return Arrays.stream(response.split(", ")).toList();
     }
 }
--- Infrastructure-Module/OpenAI/src/main/java/com/canvas/openai/gpt/DiaryContentConvertGptAdapter.java
+++ Infrastructure-Module/OpenAI/src/main/java/com/canvas/openai/gpt/DiaryContentConvertGptAdapter.java
@@ -3,8 +3,8 @@ package com.canvas.openai.gpt;
 import com.canvas.application.diary.port.out.DiaryEmotionExtractPort;
 import com.canvas.application.diary.port.out.DiaryKeywordExtractPort;
 import com.canvas.application.image.port.out.ImagePromptGeneratePort;
+import com.canvas.application.image.port.out.PromptConsts;
 import com.canvas.domain.diary.enums.Emotion;
-import com.canvas.openai.gpt.service.GptPromptConsts;
 import com.canvas.openai.gpt.service.GptService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,10 +14,10 @@ import org.springframework.stereotype.Component;
 import java.util.Arrays;
 import java.util.List;
 
+@Primary
 @Slf4j
 @Component
 @RequiredArgsConstructor
-@Primary
 public class DiaryContentConvertGptAdapter
         implements DiaryEmotionExtractPort, ImagePromptGeneratePort, DiaryKeywordExtractPort {
 
@@ -25,18 +25,18 @@ public class DiaryContentConvertGptAdapter
 
     @Override
     public Emotion emotionExtract(String content) {
-        String emotion = gptService.generate(GptPromptConsts.EMOTION_EXTRACT + content);
+        String emotion = gptService.generate(PromptConsts.EMOTION_EXTRACT + content);
         return Emotion.parse(emotion);
     }
 
     @Override
-    public String generatePrompt(String content) {
-        return gptService.generate(GptPromptConsts.IMAGE_GENERATOR + content);
+    public String generatePrompt(String content, String joinedWeightedContents) {
+        return gptService.generate(String.format(PromptConsts.IMAGE_GENERATOR, content, joinedWeightedContents));
     }
 
     @Override
     public List<String> keywordExtract(String content) {
-        String response = gptService.generate(GptPromptConsts.KEYWORD_EXTRACT + content);
+        String response = gptService.generate(PromptConsts.KEYWORD_EXTRACT + content);
         return Arrays.stream(response.split(", ")).toList();
     }
 }
